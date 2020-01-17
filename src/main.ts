b2cfdfb37c50796b76914ce43b7ef3f2184770a0
Kao Udomsub Naikorn <udomsub.nk@gmail.com>
2020-01-17 16:16:06 +0700

    :heavy_plus_sign: Set up CORS and BodyParser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+  app.use(bodyParser.json());
   await app.listen(3000);
 }
 bootstrap();
