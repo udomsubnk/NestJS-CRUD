e8f5cfaa63a2e0cad126d1179b6585b1d015f4e8
Kao Udomsub Naikorn <udomsub.nk@gmail.com>
2020-01-21 07:34:15 +0700

    :art: [Assessment] Create API for get assessments

--- src/assessment/assessment.controller.ts
+++ src/assessment/assessment.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 import { AssessmentService } from './assessment.service';
 
 @Controller('assessment')
@@ -8,5 +8,15 @@ export class AssessmentController {
   @Get('/')
   getAssessments(@Query() { page = 1 }) {
     return this.assessmentService.getAssessments(page);
+  }
+
+  @Get('/student/:studentName')
+  filterByStudentName(@Query() { page = 1 }, @Param('studentName') studentName: string) {
+    return this.assessmentService.filterByStudentName(page, studentName);
+  }
+
+  @Get('/course/:courseName')
+  filterByCourseName(@Query() { page = 1 }, @Param('courseName') courseName: string) {
+    return this.assessmentService.filterByCourseName(page, courseName);
   }
 }
--- src/assessment/assessment.service.ts
+++ src/assessment/assessment.service.ts
@@ -4,6 +4,8 @@ import { Repository, Like } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as DATABASE_QUERY from '../../constraints/DATABASE_QUERY.json';
 import { StudentEntity } from 'src/student/student.entity';
+import { UtilityFunctions } from 'src/helpers/utility';
+import { CourseEntity } from 'src/course/course.entity';
 
 @Injectable()
 export class AssessmentService {
@@ -13,11 +15,14 @@ export class AssessmentService {
 
     @InjectRepository(StudentEntity)
     private studentRepository: Repository<StudentEntity>,
+
+    @InjectRepository(CourseEntity)
+    private courseRepository: Repository<CourseEntity>,
   ) {}
 
   async getAssessments(page: number) {
     const STUDENT_LIMIT = DATABASE_QUERY.ASSESSMENT.LIMIT_STUDENT_PER_QUERY;
-    const skip = page >= 1 ? STUDENT_LIMIT * (page - 1) : 0;
+    const skip = UtilityFunctions.calculateDatabaseQueryOffset(page, STUDENT_LIMIT);
 
     const students = await this.studentRepository.find({
       order: { id: 'ASC' },
@@ -31,15 +36,70 @@ export class AssessmentService {
       .createQueryBuilder('assessments')
       .leftJoinAndSelect('assessments.course', 'courses')
       .where('assessments.student_id IN (:...studentIds)', { studentIds })
+      .orderBy('assessments.course_id', 'ASC')
       .getMany();
 
     const studentsAssessments = students.map(student => ({
       ...student,
-      assessments: assessments
-        .filter(({ student_id }) => student_id === student.id)
-        .map(({ course }) => course),
+      assessments: assessments.filter(({ student_id }) => student_id === student.id),
     }));
 
-    return studentsAssessments;
+    return { students: studentsAssessments, current_page: page };
+  }
+
+  async filterByStudentName(page: number, studentName: string) {
+    const STUDENT_LIMIT = DATABASE_QUERY.ASSESSMENT.LIMIT_STUDENT_PER_QUERY;
+    const skip = UtilityFunctions.calculateDatabaseQueryOffset(page, STUDENT_LIMIT);
+
+    const students = await this.studentRepository.find({
+      where: [{ name: Like(`%${studentName}%`) }],
+      order: { id: 'ASC' },
+      take: STUDENT_LIMIT,
+      skip,
+    });
+
+    const studentIds = students.map(({ id }) => id);
+
+    const assessments = await this.assessmentRepository
+      .createQueryBuilder('assessments')
+      .leftJoinAndSelect('assessments.course', 'courses')
+      .where('assessments.student_id IN (:...studentIds)', { studentIds })
+      .orderBy('assessments.course_id', 'ASC')
+      .getMany();
+
+    const studentsAssessments = students.map(student => ({
+      ...student,
+      assessments: assessments.filter(({ student_id }) => student_id === student.id),
+    }));
+
+    return { students: studentsAssessments, current_page: page };
+  }
+
+  async filterByCourseName(page: number, courseName: string) {
+    const STUDENT_LIMIT = DATABASE_QUERY.ASSESSMENT.LIMIT_COURSE_PER_QUERY;
+    const skip = UtilityFunctions.calculateDatabaseQueryOffset(page, STUDENT_LIMIT);
+
+    const courses = await this.courseRepository.find({
+      where: [{ name: Like(`%${courseName}%`) }],
+      order: { id: 'ASC' },
+      take: STUDENT_LIMIT,
+      skip,
+    });
+
+    const courseIds = courses.map(({ id }) => id);
+
+    const assessments = await this.assessmentRepository
+      .createQueryBuilder('assessments')
+      .leftJoinAndSelect('assessments.student', 'students')
+      .where('assessments.course_id IN (:...courseIds)', { courseIds })
+      .orderBy('assessments.student_id', 'ASC')
+      .getMany();
+
+    const coursesAssessments = courses.map(course => ({
+      ...course,
+      enrolled_students: assessments.filter(({ course_id }) => course_id === course.id),
+    }));
+
+    return { courses: coursesAssessments, current_page: page };
   }
 }
